f4282b9a6a0dc09ed677495f094dbf4a5191acf6
shivam <shivamg11000code@gmail.com>
2019-12-30 15:02:36 +0530

    started using react-intl

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {IntlProvider, FormattedMessage} from 'react-intl';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Eric',
+      unreadCount: 1000,
+    };
+  }
+
+  render() {
+    const {name, unreadCount} = this.state;
+
+    return (
+      <IntlProvider locale="en">
+      <p>
+        <FormattedMessage
+          id="welcome"
+          defaultMessage={`Hello {name}, you have {unreadCount, number} {unreadCount, plural,
+                      one {message}
+                      other {messages}
+                    }`}
+          values={{name: <b>{name}</b>, unreadCount}}
+        />
+      </p>
+      </IntlProvider>
+    );
+  }
 }
 
 export default App;
